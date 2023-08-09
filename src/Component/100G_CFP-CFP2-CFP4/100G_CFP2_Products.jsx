939a430208652d6575ca2acc79c1f106d263f82f
vijayK006 <vijaykale7052@gmail.com>
2023-08-09 16:47:10 +0530

    100G folder Images  added

--- src/Component/100G_CFP-CFP2-CFP4/100G_CFP2_Products.jsx
+++ src/Component/100G_CFP-CFP2-CFP4/100G_CFP2_Products.jsx
@@ -15,7 +15,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-import productImg1 from '../../img/products/40GLR41.jpg'
+import productImg1 from '../../img/products/100G CFP CFP2 CFP4/100GCFP2.jpg'
+import productImg2 from '../../img/products/100G CFP CFP2 CFP4/100G_ER4DuleRate.jpg'
+import productImg3 from '../../img/products/100G CFP CFP2 CFP4/100G_L4_Eathernet.png'
+import productImg4 from '../../img/products/100G CFP CFP2 CFP4/100G_SR10DuleRate.jpg'
 
 import { TfiHeadphoneAlt } from 'react-icons/tfi';
 
@@ -60,20 +63,88 @@ const CFP2_Products_100G = () => {
       <div className='row pt-5  pb-5'>
 
         <div className='col-lg-4 col-md-6 col-sm-12'>
-        <Link to="/LR4_10KM_40G" style={{textDecoration:"none", color:"black"}}>
+        <Link to="" style={{textDecoration:"none", color:"black"}}>
 
 <div className='product-card position-relative'>
 <img src={productImg1} className='productImg3'/>
 
 <div className='product-dec'>
-  <h5>40G QSFP+ LR4 Transceiver
-Hot Pluggable, Duplex LC, CWDM DFB, SMF 10KM, DDM
+  <h5>100G CFP2 to QSFP28 Adapter Module, Dual-Rate
 </h5>
 
 <h4>Feature's</h4>
-<p>Supports QDR / DDR InfiniBand</p>
+<p>Single +3.3V power supply</p>
 <p>Hot Pluggable</p>
-<p>Duplex LC connector</p>
+<p>1 port QSFP28 TX&RX</p>
+<p>RoHS compliant</p>
+</div>
+
+<TfiHeadphoneAlt className="enquire-btn" data-bs-toggle="modal" data-bs-target="#myModal"/>
+</div>
+</Link>
+
+        </div>
+
+        <div className='col-lg-4 col-md-6 col-sm-12'>
+        <Link to="" style={{textDecoration:"none", color:"black"}}>
+
+<div className='product-card position-relative'>
+<img src={productImg2} className='productImg3'/>
+
+<div className='product-dec'>
+  <h5>100G CFP2 to QSFP28 Adapter Module, Dual-Rate
+</h5>
+
+<h4>Feature's</h4>
+<p>Single +3.3V power supply</p>
+<p>Hot Pluggable</p>
+<p>1 port QSFP28 TX&RX</p>
+<p>RoHS compliant</p>
+</div>
+
+<TfiHeadphoneAlt className="enquire-btn" data-bs-toggle="modal" data-bs-target="#myModal"/>
+</div>
+</Link>
+
+        </div>
+
+        <div className='col-lg-4 col-md-6 col-sm-12'>
+        <Link to="" style={{textDecoration:"none", color:"black"}}>
+
+<div className='product-card position-relative'>
+<img src={productImg3} className='productImg3'/>
+
+<div className='product-dec'>
+  <h5>100G CFP2 to QSFP28 Adapter Module, Dual-Rate
+</h5>
+
+<h4>Feature's</h4>
+<p>Single +3.3V power supply</p>
+<p>Hot Pluggable</p>
+<p>1 port QSFP28 TX&RX</p>
+<p>RoHS compliant</p>
+</div>
+
+<TfiHeadphoneAlt className="enquire-btn" data-bs-toggle="modal" data-bs-target="#myModal"/>
+</div>
+</Link>
+
+        </div>
+
+        <div className='col-lg-4 col-md-6 col-sm-12'>
+        <Link to="" style={{textDecoration:"none", color:"black"}}>
+
+<div className='product-card position-relative'>
+<img src={productImg3} className='productImg3'/>
+
+<div className='product-dec'>
+  <h5>100G CFP2 to QSFP28 Adapter Module, Dual-Rate
+</h5>
+
+<h4>Feature's</h4>
+<p>Single +3.3V power supply</p>
+<p>Hot Pluggable</p>
+<p>1 port QSFP28 TX&RX</p>
 <p>RoHS compliant</p>
 </div>
 
@@ -85,26 +156,28 @@ Hot Pluggable, Duplex LC, CWDM DFB, SMF 10KM, DDM
 
 
         <div className='col-lg-4 col-md-6 col-sm-12'>
-        <Link to="/LR4_20KM_40G" style={{textDecoration:"none", color:"black"}}>
-        <div className='product-card position-relative'>
-<img src={productImg1} className='productImg3'/>
+        <Link to="" style={{textDecoration:"none", color:"black"}}>
+
+<div className='product-card position-relative'>
+<img src={productImg4} className='productImg3'/>
 
 <div className='product-dec'>
-  <h5>40G QSFP+ LR4 Transceiver
-Hot Pluggable, Duplex LC, CWDM DFB, SMF 20KM, DDM
+  <h5>100G CFP2 to QSFP28 Adapter Module, Dual-Rate
 </h5>
 
 <h4>Feature's</h4>
-<p>Supports QDR / DDR InfiniBand</p>
-<p>Single 3.3V power supply</p>
-<p>Duplex LC connector</p>
+<p>Single +3.3V power supply</p>
+<p>Hot Pluggable</p>
+<p>1 port QSFP28 TX&RX</p>
 <p>RoHS compliant</p>
 </div>
 
 <TfiHeadphoneAlt className="enquire-btn" data-bs-toggle="modal" data-bs-target="#myModal"/>
 </div>
-        </Link>
+</Link>
+
         </div>
+
       </div>
 </div>
 
