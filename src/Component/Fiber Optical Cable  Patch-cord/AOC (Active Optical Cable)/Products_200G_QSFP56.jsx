96967f7708acfa2af839ec0b919830cd95e81be9
Akashshripal <akashshripal786@gmail.com>
2023-08-29 14:37:24 +0530

    pushing code

--- src/Component/Fiber Optical Cable  Patch-cord/AOC (Active Optical Cable)/Products_200G_QSFP56.jsx
+++ src/Component/Fiber Optical Cable  Patch-cord/AOC (Active Optical Cable)/Products_200G_QSFP56.jsx
@@ -15,13 +15,13 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-import productImg1 from '../../../img/products/Fiber Optical Cable/AOC/QSFP28.jpg'
-import productImg2 from '../../../img/products/Fiber Optical Cable/AOC/QPSP2.jpg';
-import productImg3 from '../../../img/products/Fiber Optical Cable/AOC/QPSP.jpg';
+import productImg1 from '../../../img/products/Fiber Optical Cable/AOC/Q5Q5.jpg'
+import productImg2 from '../../../img/products/Fiber Optical Cable/AOC/Q6Q6.jpg';
+import productImg3 from '../../../img/products/Fiber Optical Cable/AOC/Q6Q62.jpg';
 
 import { TfiHeadphoneAlt } from 'react-icons/tfi';
 
-const Products_100G_QSFP28 = () => {
+const Products_200G_QSFP56 = () => {
   return (
     <>
 <Navbar/>
@@ -57,24 +57,25 @@ const Products_100G_QSFP28 = () => {
       </Swiper>
 
 
-<h2 className='pt-5'>100G QSFP28 AOC</h2>
+<h2 className='pt-5'>200G QSFP56 AOC</h2>
 
       <div className='row pt-5  pb-5'>
 
       <div className='col-lg-4 col-md-6 col-sm-12'>
-        <Link to="/Products_100G_QSFP28/QSFP28_QPQP_100G" style={{textDecoration:"none", color:"black"}}>
+        <Link to="/Products_200G_QSFP56/QSFP56_Q5Q5_200G" style={{textDecoration:"none", color:"black"}}>
 
 <div className='product-card position-relative'>
 <img src={productImg1} className='productImg3'/>
 
 <div className='product-dec'>
   <h5>
-  100G QSFP28 Active Optical Cable ( AOC )
-Hot Pluggable, 850nm VCSEL, MMF 1~100M, DDM
+  200G QSFP56 Active Optical Cable ( AOC )
+Hot Pluggable, 850nm VCSEL, MMF OM4 1~100M, DDM
+
 
 
 </h5>
-<p>Part Number : NXOAOC-A0G-QPQP-xxx-xx
+<p>Part Number : NXOAOC-B0G-Q5Q5-xxx-xx
 </p>
 
 <h4>Feature's</h4> 
@@ -98,21 +99,21 @@ Hot Pluggable, 850nm VCSEL, MMF 1~100M, DDM
 
         </div>
         
-
         <div className='col-lg-4 col-md-6 col-sm-12'>
-        <Link to="/Products_100G_QSFP28/QSFP28_QPSP_100G" style={{textDecoration:"none", color:"black"}}>
+        <Link to="/Products_200G_QSFP56/QSFP56_Q6Q5_200G" style={{textDecoration:"none", color:"black"}}>
 
 <div className='product-card position-relative'>
 <img src={productImg2} className='productImg3'/>
 
 <div className='product-dec'>
   <h5>
-  100G QSFP28 to 4x25G SFP28 Breakout Active Optical Cable ( AOC ) Hot Pluggable, 850nm VCSEL, MMF 1~100M, DDM
-s
+  200G QSFP56 to 2x100G QSFP56 Active Optical Cable ( AOC ) Hot Pluggable, 850nm VCSEL, MMF 1~100M, DDM
+
+
 
 
 </h5>
-<p>Part Number : NXOAOC-A0G-QPSP-xxx-xx
+<p>Part Number : NXOAOC-B0G-Q6Q5-xxx-xx
 </p>
 
 <h4>Feature's</h4> 
@@ -134,7 +135,46 @@ s
 </div>
 </Link>
 
-        </div> 
+        </div>
+       
+        <div className='col-lg-4 col-md-6 col-sm-12'>
+        <Link to="/Products_200G_QSFP56/QSFP56_Q6Q6_200G" style={{textDecoration:"none", color:"black"}}>
+
+<div className='product-card position-relative'>
+<img src={productImg3} className='productImg3'/>
+
+<div className='product-dec'>
+  <h5>
+  200G QSFP56 Active Optical Cable ( AOC )
+Hot Pluggable, 850nm VCSEL, MMF 1~100M, DDM
+
+
+
+
+</h5>
+<p>Part Number : NXOAOC-B0G-Q6Q6-xxx-xx
+</p>
+
+<h4>Feature's</h4> 
+
+<p>Hot Pluggable
+
+</p>
+
+<p>Single 3.3V power supply
+
+</p>
+<p>RoHS Compliant
+</p>
+
+
+</div>
+
+<TfiHeadphoneAlt className="enquire-btn" data-bs-toggle="modal" data-bs-target="#myModal"/>
+</div>
+</Link>
+
+        </div>
       </div>
 </div>
 
@@ -145,4 +185,4 @@ s
   )
 }
 
-export default Products_100G_QSFP28;
+export default Products_200G_QSFP56;
